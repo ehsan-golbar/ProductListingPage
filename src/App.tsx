e09367a8b3e642359733b79befb30289a9c0a928
Ehsan <1382ehg@gmail.com>
2024-08-22 19:07:02 +0330

    fix buld issu

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 // import { Col, Container, Row } from 'react-bootstrap'
 import ProductCard from './components/card/ProductCard'
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
 
 function App() {
