df1f50f6196c35809021a1ab150991d6ea8a9ef6
Ehsan <1382ehg@gmail.com>
2024-08-22 22:47:35 +0330

    add search pannel

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
-
 // import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 // import { Col, Container, Row } from 'react-bootstrap'
-import Home from './components/home/Home'
-import {  Stack } from "react-bootstrap"
-import Header from "./components/header/Header"
+import Home from "./components/home/Home";
+import {  Stack } from "react-bootstrap";
+import Header from "./components/header/Header";
+import SearchPannel from "./components/search/SearchPannel";
 // import { useEffect } from 'react'
 
-
 function App() {
-
-
-
-
-
-
   return (
     <>
-      <Stack style={{ width: '100vw' }}>
-        {/* <ProductCard>
-          
-        </ProductCard> */}
-        <Header></Header>
-        <Home></Home>
+      <Header></Header>
+
+      <Stack style={{ width: "100vw" }}>
+        <div className="d-flex">
+          <div className="flex-shrink-0" style={{ width: "25%" }}>
+            <Stack>
+              {/* Content for the first Stack */}
+              <SearchPannel></SearchPannel>
+            </Stack>
+          </div>
+          <div className="flex-grow-1" style={{ width: "75%" }}>
+            <Stack>
+              {/* Content for the second Stack */}
+              <Home></Home>
+            </Stack>
+          </div>
+        </div>
       </Stack>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/search/SearchPannel.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "react-bootstrap";
+
+function SearchPannel() {
+    return(
+<>
+<Stack >
+
+        <p>slam</p>
+</Stack>
+
+</>
+
+    )
+}
+
+export default SearchPannel
